8f2ecaedf18bdbfe3f2e308b28218ae222d6e835
Yago <yagoalmeida21@live.com>
2022-03-22 22:57:41 -0300

    atividades sql gen

--- Ex2103/ex3.sql
+++ Ex2103/ex3.sql
@@ -4,83 +4,75 @@ USE db_farmacia_do_bem;
 
 CREATE TABLE tb_categoria(
     id bigint auto_increment,
-    cosmetico varchar(255),
-    higiene varchar(255),
-	ansiolitico varchar(255),
-	outros varchar(255),
-    
+    tipo varchar(255),
+    descricao varchar(255),
+    receita boolean,
 	primary key(id)
-);
-
-SELECT * FROM tb_produto;
-
-CREATE TABLE tb_categoria(
-    id bigint auto_increment,
-	
-    
-    
-    primary key(id)
-    
 );
 
 SELECT * FROM tb_categoria;
 
+CREATE TABLE tb_produto(
+    id bigint auto_increment,
+	nome varchar(255),
+	validade date,
+    preco decimal(4,2),
+	categoria_id bigint,
+  
+    primary key(id),
+    foreign key(categoria_id) references tb_categoria(id)
+);
+
+SELECT * FROM tb_produto;
 
 
-INSERT INTO tb_pizza(sabor, preco, borda)
-VALUES("Calabresa", 40 , 1);
 
-INSERT INTO tb_pizza(sabor, preco, borda)
-VALUES("Chocolate", 45 , 0);
+INSERT INTO tb_categoria(tipo, descricao, receita)
+VALUES("Antidepressivo", "Controla impulsos maniacos", 1);
 
-INSERT INTO tb_pizza(sabor, preco, borda)
-VALUES("Portuguesa", 42 , 1);
+INSERT INTO tb_categoria(tipo, descricao, receita)
+VALUES("Ansiolítico", "Controla a ansiedade", 1);
 
-INSERT INTO tb_pizza(sabor, preco, borda)
-VALUES("Banana", 35 ,  0);
+INSERT INTO tb_categoria(tipo, descricao, receita)
+VALUES("Pomada antibatcteriana", "Controla o surgimento de novas bactérias inflamatorias", 0);
 
-INSERT INTO tb_pizza(sabor, preco, borda)
-VALUES("Marguerita", 42 , 1);
+INSERT INTO tb_categoria(tipo, descricao, receita)
+VALUES("Shampoo", "Anticaspa", 0);
 
-INSERT INTO tb_pizza(sabor, preco, borda)
-VALUES("Pepperoni", 48 , 0);
-
-INSERT INTO tb_pizza(sabor, preco, borda)
-VALUES("Brocolis", 40 , 1);
-
-INSERT INTO tb_pizza(sabor, preco, borda)
-VALUES("4 queijos", 43 , 0);
+INSERT INTO tb_categoria(tipo, descricao, receita)
+VALUES("Antipiretico", "Controla a febre", 0);
 
 -- _____________________________________________________________
 
-INSERT INTO tb_categoria(doce, tamanho)
-VALUES(1, "Media");
+INSERT INTO tb_produto(nome, validade, preco)
+VALUES("Rivotril", "2023-10-12", 33.99);
 
-INSERT INTO tb_categoria(doce, tamanho)
-VALUES (0, "Grande");
+INSERT INTO tb_produto(nome, validade, preco)
+VALUES("Amoxicilina", "2024-09-12", 22.99);
 
-INSERT INTO tb_categoria(doce, tamanho)
-VALUES(0, "Familia");
+INSERT INTO tb_produto(nome, validade, preco)
+VALUES("Clear", "2023-07-09", 17.99);
+
+INSERT INTO tb_produto(nome, validade, preco)
+VALUES("Nebacetin", "2024-08-23", 14.99);
+
+INSERT INTO tb_produto(nome, validade, preco)
+VALUES("Dipirona Monoidratada", "2023-06-09", 33.99);
+
+INSERT INTO tb_produto(nome, validade, preco)
+VALUES("Head n Shoulders", "2023-04-06", 16.99);
+
 
 -- _______________________________________________________________
 
 
--- alterei o valor da pizza para o select abaixo funcionar na varredura
-UPDATE tb_pizza SET preco = 48 WHERE ID = 6; 	
-
-SELECT * FROM tb_pizza WHERE preco > 45;
-SELECT * FROM tb_pizza WHERE preco BETWEEN 29 AND 60;
+SELECT * FROM tb_produto WHERE preco > 50;
+SELECT * FROM tb_produto WHERE preco BETWEEN 3 AND 60;
 
 
-SELECT * FROM tb_pizza WHERE nome LIKE "%c%";
-SELECT * FROM tb_categoria WHERE nome LIKE "%c%";
+SELECT * FROM tb_produto WHERE nome LIKE "%b%";
 
-SELECT * FROM
-tb_pizza INNER JOIN tb_categoria
-ON tb_pizza.id = tb_categoria.pizza_id;
+SELECT * FROM tb_produto 
+INNER JOIN tb_categoria on tb_categoria.id = tb_produto.id;
 
-
-SELECT *
-FROM tb_categoria INNER JOIN tb_pizza 
-ON tb_pizza.id = tb_categoria.pizza_id
-WHERE categoria.id = 2;
+SELECT * FROM tb_produto WHERE tb_categoria.id = 3 AND tb_categoria.id = 4;
